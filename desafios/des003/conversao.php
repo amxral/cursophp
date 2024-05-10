69a7c8099f4c2b4692424ecedfdcaed1ad6e0ab9
amaral <119061935+amxral@users.noreply.github.com>
2024-05-10 20:39:01 -0300

    conversor de moedas simples

--- desafios/des003/conversao.php
+++ desafios/des003/conversao.php
@@ -1,16 +1,27 @@
-<?php 
-    //Cotação copiada do google
-    $cotacao = 5.16;
-
-    //Quanto $$ você tem?
-    $real = 1000;
-
-    //Equivalêcia em dólar
-    $dolar = $real / $cotacao;
-
-    //Formatação de moedas com Internacionalização
-    //Biblioteca intl (Internacionalização PHP)
-    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-
-    echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD");
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Conversor de Moedas</title>
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php
+            //Cotação copiada do google
+            $cotacao = 5.16;
+            //Quanto $$ você tem?
+            $real = $_GET['din'];
+            //Equivalêcia em dólar
+            $dolar = $real / $cotacao;
+            //Formatação de moedas com Internacionalização
+            //Biblioteca intl (Internacionalização PHP)
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD") . "</p>";
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
