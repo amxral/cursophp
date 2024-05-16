0bf4499799d4ccbe562c3bfb1f0335ee264999b6
amaral <119061935+amxral@users.noreply.github.com>
2024-05-16 19:52:35 -0300

    create new code

---
+++ desafios/des005/numero.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Resposta</title>
+</head>
+<body>
+    <main>
+        <h1>Analisador de Número Real</h1>
+        <?php 
+            $num = $_POST["n"] ?? 0;
+
+            echo "Analisando o número <strong>$num</strong> informado pelo usuário.."
+        ?>
+    </main>
+</body>
+</html>
